26e7bcc11ec5f50e1a03e4358dc4fd0c12f0e591
Mate Solymosi <mate@solymosi.eu>
2011-10-14 22:27:36 +0200

    Improved error screen.

--- include/modules/errorMessage.php
+++ include/modules/errorMessage.php
@@ -1,3 +1,22 @@
-<h1>Jaj! Valami baj van!</h1>
-
-<p><?= $e->getMessage() ?></p>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Fatal Error</title>
+		<style type="text/css">
+			body { font-family: arial,helvetica,sans-serif; }
+			h1 { color: #D00; letter-spacing: -0.05em; }
+			small { color: #999; font-size: 0.7em }
+		</style>
+	</head>
+	
+	<body>
+		
+		<h1>EPIC FAIL :(</h1>
+		
+		<p><strong>The system encountered a fatal error.</strong></p>
+		
+		<p><small><?= Helpers::h($e->getMessage()) ?></small></p>
+		
+	</body>
+</html>
