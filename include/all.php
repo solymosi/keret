3556574ff6a9cbf2598f7bae1442e667aa98811b
Mate Solymosi <mate@solymosi.eu>
2011-10-14 16:44:41 +0200

    Added initial framework files.

---
+++ include/all.php
@@ -0,0 +1,14 @@
+<?php
+
+	// Load all shared files at once
+
+	require_once("./configuration.php");
+	require_once("./database.php");
+	require_once("./user.php");
+	require_once("./session.php");
+	require_once("./validation.php");
+	require_once("./helpers.php");
+	require_once("./routing.php");
+	require_once("./facebook.php");
+
+?>
