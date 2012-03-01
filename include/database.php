79f7763bda3022a0bb1c45425ba8a5d3703e0a52
Mate Solymosi <mate@solymo.si>
2012-03-01 09:43:57 +0100

    Re-added comments to database.php and helpers.php

--- include/database.php
+++ include/database.php
@@ -2,9 +2,11 @@
 
 	class DB
 	{
-		
+
+		// Az aktív adatbáziskapcsolat
 		public static $link;
-		
+
+		// Kapcsolódik az adatbázishoz és beállítja az UTF-8-as karakterkódolást
 		public static function connect()
 		{
 			if(!self::$link = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD))
@@ -18,12 +20,20 @@
 			DB::query("set names utf8");
 		}
 		
+		// Előkészít egy paraméterezett lekérdezést a futtatáshoz (escapeli a megadott paramétereket és beszúrja őket a megfelelő helyre)
 		public static function prepare($sql, $params = array())
 		{
-			$params = array_merge(array($sql), array_map("mysql_real_escape_string", $params));
+			$params = array_merge(array($sql), array_map(array("self", "escape"), $params));
 			return call_user_func_array("sprintf", $params);
 		}
 		
+		// Escapeli a megadott sztringet
+		public static function escape($text)
+		{
+			return mysql_real_escape_string($text);
+		}
+		
+		// Futtat egy lekérdezést. Az SQL sztring után még paramétereket is meg lehet adni. Ezek automatikusan escapelve lesznek, és be lesznek szúrva az SQL sztringbe az %s-sel megjelölt helyekre.
 		public static function query($sql)
 		{
 			$params = array();
@@ -39,16 +49,19 @@
 			return $result;
 		}
 		
+		// Lekéri a megadott lekérdezési eredményben lévő sorok számát
 		public static function rows($result)
 		{
 			return @mysql_num_rows($result);
 		}
 		
+		// Visszaadja a soron következő elemet a lekérdezés eredményében, vagy false-t, ha nincs több
 		public static function fetch($result)
 		{
 			return @mysql_fetch_assoc($result);
 		}
 		
+		// Visszaadja a legutóbbi MySQL hibaüzenetet
 		public static function lastError()
 		{
 			return @mysql_error();
--- include/helpers.php
+++ include/helpers.php
@@ -2,10 +2,14 @@
 
 	class Helpers
 	{
+		// Gyorsítótárazott elemek
 		public static $baseUri = null;
 		public static $scheme = null;
 		public static $uri = null;
 		
+		// Lekéri a front controller (index.php) URL címét (ezt fel lehet használni URL-generáláskor)
+		// Példa: https://apro.kozgaz.net/index.php
+		// Forrás: Slim PHP5 framework
 		public static function getBaseUri($reload = false)
 		{
 			if($reload || is_null(self::$baseUri))
@@ -18,6 +22,8 @@
 			return self::$baseUri;
 		}
 		
+		// Lekéri az aktuális protokollt (http vs https)
+		// Forrás: Slim PHP5 framework
 		public static function getScheme( $reload = false )
 		{
 			if($reload || is_null(self::$scheme))
@@ -27,6 +33,9 @@
 			return self::$scheme;
 		}
 		
+		// Lekéri az index.php után lévő részt
+		// Példa: https://apro.kozgaz.net/index.php/login  =>  /login
+		// Forrás: Slim PHP5 framework
 		public static function getUri( $reload = false )
 		{
 			if ($reload || is_null(self::$uri))
@@ -36,16 +45,20 @@
 			return self::$uri;
 		}
 		
+		// A megadott rendszeren belüli URI-t abszolút linkké konvertálja
+		// Példa: /login  =>  https://apro.kozgaz.net/index.php/login
 		public static function link($uri)
 		{
 			return self::getBaseUri() . $uri;
 		}
 		
+		// Átirányítja a user böngészőjét egy rendszeren belüli URI-ra
 		public static function redirect($uri, $permanent = false)
 		{
 			self::externalRedirect(self::link($uri), $permanent);
 		}
 		
+		// Átirányítja a user böngészőjét egy tetszőleges URL-re, és megszakítja a program futását
 		public static function externalRedirect($url, $permanent = false)
 		{
 			Helpers::clearOutput();
@@ -56,6 +69,7 @@
 			exit;
 		}
 		
+		// Végtelen ciklus megakadályozása az exception értesítéseknél, ha az exception az üzenet küldése közben keletkezett
 		private static $sendingMail = false;
 		
 		public static function sendMail($to, $subject, $body, $additionalHeaders = "")
@@ -66,21 +80,28 @@
 			self::$sendingMail = false;
 		}
 		
+		// Megjeleníti a 404-es hibaüzenetet, és leállítja a program futását
 		public static function notFound($message = "The requested resource was not found")
 		{
 			throw new NotFoundException($message);
 		}
 		
+		// Beállítja a megadott HTTP állapotüzenetet a böngészőnek küldendő válaszban
 		public static function setStatusCode($code)
 		{
 			header($_SERVER["SERVER_PROTOCOL"] . " " . $code);
 		}
 		
+		// Visszaadja a megadott asset (kép, css, js) fájl abszolút URL-jét
+		// Példa: img/logo.png  =>  https://apro.kozgaz.net/assets/img/logo.png
 		public static function asset($name)
 		{
 			return ASSETS_URL . "/" . $name;
 		}
 		
+		// Escapeli a megadott szövegben lévő HTML-specifikus karaktereket (XSS védelem)
+		// MINDEN FELHASZNÁLÓTÓL JÖVŐ, MAJD KIPRINTELT ADATNAK ÁT KELL MENNIE EZEN KÖZVETLENÜL A PRINTELÉS ELŐTT!
+		// Példa: <script>alert('XSS FAIL')</script>  =>  &lt;script&gt;alert('XSS FAIL')&lt;/script&gt;
 		public static function h($content)
 		{
 			return htmlentities($content, ENT_QUOTES, "UTF-8");
@@ -91,16 +112,20 @@
 			return number_format($price, 0, "", ",") . ($includeCurrency ? " HUF" : "");
 		}
 		
+		// Töröl minden eddig kiprintelt tartalmat a kimeneti gyorsítótárból
 		public static function clearOutput()
 		{
+			// Az összes gyorsítótár-szint tartalmát töröljük
 			if(@ob_get_level())
 			{
 				while(@ob_end_clean());
 			}
 			
+			// Újraindítjuk a gyorsítótárazást az üres gyorsítótárral
 			ob_start();
 		}
 		
+		// Átirányítja a böngészőt az index.php-ra, ha az valami miatt nem szerepel az URL-ben (ez azért kell, mert a .htaccess csak akkor engedélyezi a lekérést, ha az index.php-val kezdődik)
 		public static function ensureCorrectBaseUri()
 		{
 			if(!preg_match("/^.*\/index\.php$/", self::getBaseUri()))
