e9aa7bf1fa0d20ebfec048f2a8b134a8596d94cc
Mate Solymosi <mate@solymo.si>
2013-03-09 20:02:37 +0100

    Added support for specifying the default item selected in select boxes.

--- include/form/field/select.php
+++ include/form/field/select.php
@@ -4,11 +4,16 @@
 	{
 		protected $options = array();
 		
-		public function __construct($name, $options = array(), $value = null, $params = array())
+		public function __construct($name, $options = array(), $blank = null, $value = null, $params = array())
 		{
 			self::whenNot(is_array($options), "The select options must be an array.");
 			
 			$this->options = Helpers::isAssoc($options) ? $options : array_combine($options, array_map("strval", $options));
+			
+			if(!is_null($blank))
+			{
+				$this->options = array("" => $blank) + $this->options;
+			}
 			
 			parent::__construct("select", $name, $value, $params);
 			
