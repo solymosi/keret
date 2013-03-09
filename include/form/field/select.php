cd7d184d2f2c1d5d22a2c80a3266fac7119f12d3
Mate Solymosi <mate@solymo.si>
2013-03-09 19:22:49 +0100

    Removed debugging code from select.php

--- include/form/field/select.php
+++ include/form/field/select.php
@@ -7,8 +7,9 @@
 		public function __construct($name, $options = array(), $value = null, $params = array())
 		{
 			self::whenNot(is_array($options), "The select options must be an array.");
+			
 			$this->options = Helpers::isAssoc($options) ? $options : array_combine($options, array_map("strval", $options));
-			var_dump($this->options);
+			
 			parent::__construct("select", $name, $value, $params);
 			
 			$this->addParams(array("class" => "text"));
