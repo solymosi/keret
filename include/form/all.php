095347750938c49f8b5e68b15bf6c646e63eb6cb
Mate Solymosi <mate@solymo.si>
2013-03-11 11:00:19 +0100

    Enhanced RangeValidator and renamed it to NumberValidator.

--- include/form/all.php
+++ include/form/all.php
@@ -34,7 +34,7 @@
 	require_once("validator/fieldEquality.php");
 	require_once("validator/inclusion.php");
 	require_once("validator/length.php");
-	require_once("validator/range.php");
+	require_once("validator/number.php");
 	require_once("validator/email.php");
 	require_once("validator/regex.php");
 	require_once("validator/date.php");
