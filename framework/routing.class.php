3150564bcb6cb1f912bdf861dcaedd4e2451447e
Mate Solymosi <mate@solymo.si>
2014-10-04 21:19:14 +0200

    Store route parameters in routing class.

--- framework/routing.class.php
+++ framework/routing.class.php
@@ -2,15 +2,52 @@
 
 	class Routing
 	{
-		public static function match($pattern, $controller, $action)
+		static protected $params = array();
+		
+		static public function match($pattern, $controller, $action, $params = array())
 		{
+			self::addParams($params);
+			
 			if(preg_match("/^\/" . $pattern . "$/", Helpers::getUri(), $matches))
 			{
-				call_user_func(array(ucfirst($controller) . "Controller", $action), $matches);
+				self::addParams($matches);
+				call_user_func(array(ucfirst($controller) . "Controller", $action), self::getParams());
 				throw new ProcessingFinished();
 			}
 		}
-	
+		
+		static public function get($key)
+		{
+			return isset(self::$params[$key]) ? self::$params[$key] : null;
+		}
+		
+		static public function set($key, $value)
+		{
+			self::$params[$key] = $value;
+		}
+		
+		static public function getParams()
+		{
+			return self::$params;
+		}
+		
+		static public function setParams($params)
+		{
+			if(!is_array($params))
+			{
+				throw new Exception("The variable passed to setParams() must be an array");
+			}
+			self::$params = $params;
+		}
+		
+		static public function addParams($params)
+		{
+			if(!is_array($params))
+			{
+				throw new Exception("The variable passed to addParams() must be an array");
+			}
+			self::setParams(array_merge(self::$params, $params));
+		}
 	}
 	
 	class ProcessingFinished extends Exception { }
