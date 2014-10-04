30984eaa3c729a449314b40f743751c9534ce292
Mate Solymosi <mate@solymo.si>
2014-10-04 16:32:51 +0200

    Use PDO for database operations.

--- include/database.php
+++ include/database.php
@@ -6,79 +6,74 @@
 
 		public static function connect()
 		{
-			if(!self::$link = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD))
-			{
-				throw new Exception("MySQL error: " . self::lastError());
-			}
-			if(!@mysql_select_db(DB_DATABASE, self::$link))
-			{
-				throw new Exception("MySQL error: " . self::lastError());
-			}
-			DB::query("set names utf8");
-			DB::query("set time_zone = '%s'", date_default_timezone_get());
+			self::$link = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";charset=utf8", DB_USER, DB_PASSWORD, array(
+				PDO::ATTR_EMULATE_PREPARES => false,
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			));
 		}
 		
 		public static function prepare($sql, $params = array())
 		{
-			$params = array_merge(array($sql), array_map(array("self", "escape"), $params));
-			return call_user_func_array("sprintf", $params);
-		}
-		
-		public static function escape($text)
-		{
-			return mysql_real_escape_string($text);
-		}
-		
-		public static function quote($text)
-		{
-			return "'" . DB::escape($text) . "'";
-		}
-		
-		public static function query($sql)
-		{
-			$params = array();
-			for($i = 1; $i < func_num_args(); $i++)
+			$stmt = self::$link->prepare($sql);
+			
+			foreach($params as $name => $value)
 			{
-				$params[] = func_get_arg($i);
+				if(mb_strpos($sql, ":" . $name) !== false)
+				{
+					$stmt->bindValue(":" . $name, $value, self::paramType($value));
+				}
 			}
-			$sql = self::prepare($sql, $params);
-			if(!$result = @mysql_query($sql, self::$link))
+			
+			return $stmt;
+		}
+		
+		protected static function paramType($param)
+		{
+			switch(strtolower(gettype($param)))
 			{
-				throw new Exception("MySQL error: " . self::lastError() . ". The query was: " . $sql);
+				case "boolean": return PDO::PARAM_BOOL;
+				case "integer": return PDO::PARAM_INT;
+				case "null": return PDO::PARAM_NULL;
+				default: return PDO::PARAM_STR;
 			}
-			return $result;
 		}
 		
-		public static function rows($result)
+		public static function query($sql, $params = array())
 		{
-			return @mysql_num_rows($result);
+			$stmt = self::prepare($sql, $params);
+			$stmt->execute();
+			
+			return $stmt;
 		}
 		
-		public static function fetch($result)
+		public static function fetch($stmt)
 		{
-			return @mysql_fetch_assoc($result);
+			return $stmt->fetch(PDO::FETCH_ASSOC);
 		}
 		
 		public static function fetchAll($result)
 		{
-			$rows = array();
-			while($row = self::fetch($result))
-			{
-				$rows[] = $row;
-			}
-			return $rows;
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
 		}
 		
-		public static function get()
+		public static function fetchValue($stmt, $column = 0)
 		{
-			$fga = func_get_args();
-			return self::fetch(call_user_func_array(array("self", "query"), $fga));
+			return $stmt->fetchColumn($column);
 		}
 		
-		public static function getAll()
+		public static function get($sql, $params = array())
 		{
-			$fga = func_get_args();
-			return self::fetchAll(call_user_func_array(array("self", "query"), $fga));
+			return self::fetch(self::query($sql, $params));
+		}
+		
+		public static function getAll($sql, $params = array())
+		{
+			return self::fetchAll(self::query($sql, $params));
+		}
+		
+		public static function getValue($sql, $params = array(), $column = 0)
+		{
+			return self::fetchValue(self::query($sql, $params), $column);
 		}
 		
 		public static function buildAssignment($data, $filter = null)
@@ -91,7 +86,7 @@
 			$parts = array();
 			foreach($data as $field => $value)
 			{
-				$parts[] = $field . " = " . (is_null($value) ? "null" : "'" . str_replace("%", "%%", self::escape($value)) . "'");
+				$parts[] = $field . " = :" . $field;
 			}
 			
 			return implode(", ", $parts);
@@ -99,13 +94,7 @@
 		
 		public static function lastID()
 		{
-			$row = self::get("select last_insert_id() as id");
-			return $row["id"];
-		}
-		
-		public static function lastError()
-		{
-			return @mysql_error();
+			return self::$link->lastInsertId();
 		}
 	}
 
