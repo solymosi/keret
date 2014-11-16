84fe41793afcb2c4c9cc15748ae23a3e5e5119b1
Mate Solymosi <mate@solymo.si>
2014-11-16 03:39:49 +0100

    Added a couple of field classes.

---
+++ framework/form/fields/hiddenField.class.php
@@ -0,0 +1,9 @@
+<?php
+
+	class HiddenField extends Field
+	{
+		public function __construct($name, $value = null, $params = array())
+		{
+			parent::__construct($name, $value, $params);
+		}
+	}
---
+++ framework/form/fields/submitButton.class.php
@@ -0,0 +1,9 @@
+<?php
+
+	class SubmitButton extends Button
+	{
+		public function __construct($label, $name = null, $params = array())
+		{
+			parent::__construct($label, $name, $params);
+		}
+	}
