b38167ed3ba0c06b02bf54a565334252bbcbfb61
Mate Solymosi <mate@solymo.si>
2014-11-16 03:36:10 +0100

    Autoload form framework subfolders.

--- framework/boot.php
+++ framework/boot.php
@@ -5,5 +5,8 @@
 	Autoload::initialize();
 	Autoload::registerPath(dirname(__FILE__));
 	Autoload::registerPath(dirname(__FILE__) . "/form");
+	Autoload::registerPath(dirname(__FILE__) . "/form/fields");
+	Autoload::registerPath(dirname(__FILE__) . "/form/renderers");
+	Autoload::registerPath(dirname(__FILE__) . "/form/validators");
 	Autoload::registerPath(dirname(__FILE__) . "/i18n");
 	Autoload::registerPath(dirname(__FILE__) . "/vendor");
