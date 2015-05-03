1956e1fc4c40a2987721ff5830774fa041ac7ac6
Mate Solymosi <mate@solymo.si>
2015-05-03 06:07:44 +0200

    Removed unnecessary files.

--- _legacy/form/hiddenField.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	class HiddenField extends InputField
-	{
-		public function __construct($name, $value = null, $params = array())
-		{
-			parent::__construct("hidden", $name, $value, $params);
-		}
-	}
