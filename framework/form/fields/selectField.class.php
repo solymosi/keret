88612c55d99b4e90cf2d8e1133db0fe1e4b24c08
Mate Solymosi <mate@solymo.si>
2015-11-28 05:37:49 +0100

    Added support for option groups in select fields.

--- framework/form/fields/selectField.class.php
+++ framework/form/fields/selectField.class.php
@@ -2,8 +2,26 @@
 
 	class SelectField extends ListField
 	{
+		protected $groups = array();
+		
 		public function __construct($name, $items = array(), $value = null, $params = array())
 		{
 			parent::__construct($name, $items, $value, $params);
+		}
+		
+		public function getGroups()
+		{
+			return $this->groups;
+		}
+		
+		public function setItems($items)
+		{
+			parent::setItems(
+				Helpers::flatten($items)
+			);
+			
+			$this->groups = $items;
+			
+			return $this;
 		}
 	}
--- framework/form/renderers/selectFieldRenderer.class.php
+++ framework/form/renderers/selectFieldRenderer.class.php
@@ -13,20 +13,35 @@
 			
 			return $this->tag(
 				"select",
-				implode("", array_map(
-					function($key) use ($field) {
-						return $this->tag(
+				$this->renderOptions(),
+				$this->fieldParams()
+			);
+		}
+		
+		protected function renderOptions($items = null)
+		{
+			$field = $this->getField();
+			$items = is_null($items) ? $field->getGroups() : $items;
+			
+			return implode("", array_map(
+				function($key) use($field, $items) {
+					$label = $items[$key];
+					return is_array($label) ?
+						$this->tag(
+							"optgroup",
+							$this->renderOptions($label),
+							array("label" => $key)
+						) :
+						$this->tag(
 							"option",
-							Helpers::escapeHtml($field->getItems()[$key]),
+							Helpers::escapeHtml($label),
 							array(
 								"value" => $key,
 								"selected" => $field->getValue() == $key ? "selected" : null,
 							)
 						);
-					},
-					array_keys($field->getItems())
-				)),
-				$this->fieldParams()
-			);
+				},
+				array_keys($items)
+			));
 		}
 	}
